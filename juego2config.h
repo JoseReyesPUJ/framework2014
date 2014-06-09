6deced8775f61837c9a94be65c982a8017b682b9
JoseReyesPUJ <jreyes66@javerianacali.edu.co>
2014-06-08 19:00:51 -0500

    VERSION FINAL!!
    VERSION FINAL!!

--- juego2config.h
+++ juego2config.h
@@ -1,7 +1,10 @@
 #ifndef JUEGO2CONFIG_H
 #define JUEGO2CONFIG_H
-
 #include <QWidget>
+#include<QLineEdit>
+#include <vector>
+#include "matrizjimages.h"
+using namespace std;
 
 namespace Ui {
 class juego2config;
@@ -14,10 +17,19 @@ class juego2config : public QWidget
 public:
     explicit juego2config(QWidget *parent = 0);
     ~juego2config();
-    explicit QLineEdit **matrizTL;
+    void pasoClase(MatrizJimages*);
+
+public slots:
+    void atrasVCjuego2();
+    void continuarVCjuego2();
 
 private:
     Ui::juego2config *ui;
+    QLineEdit ***matrizTL;
+    vector<QString> vectorPalabras_;
+    MatrizJimages *claseImagenes;
+
+
 };
 
 #endif // JUEGO2CONFIG_H
