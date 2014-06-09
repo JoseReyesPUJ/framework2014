6c83dbd2fba4eb0ccc0d7c40642c2ebb4ffa729b
JoseReyesPUJ <jreyes66@javerianacali.edu.co>
2014-06-08 19:21:19 -0500

    NO INCLUIR!
    NO INCLUIR!

--- VentanaMatrizJimages.cpp
+++ VentanaMatrizJimages.cpp
@@ -1,15 +1,1 @@
-#include "matrizjimages.h"
-#include "ui_matrizjimages.h"
-
-MatrizJimages::MatrizJimages(QWidget *parent) :
-    QWidget(parent),
-    ui(new Ui::MatrizJimages)
-{
-    ui->setupUi(this);
-}
-
-MatrizJimages::~MatrizJimages()
-{
-    delete ui;
-}
-
+#
